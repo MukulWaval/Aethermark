c91bfc283f51cc59bbd279ecea95bc30ecf29140
MukulWaval <mukulwaval2000@gmail.com>
2025-03-24 17:12:00 +0530

    refactor: clean codebase

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,5 @@
 import aethermark as m
 
-def test_main():
-    print("Imported from:", m.__file__)
-    assert m.__version__ == '0.1.0'
-    assert m.add(1, 2) == 3
-    assert m.subtract(1, 2) == -1
+def test_version():
+    assert m.__version__ == '0.1.4'
+
